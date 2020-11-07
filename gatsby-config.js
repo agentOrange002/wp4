f98a561ef29a5c83235a599c06a8f232262188eb
agentOrange002 <nehemiasbelong@gmail.com>
2020-11-07 20:27:36 +0800

    132123

--- gatsby-config.js
+++ gatsby-config.js
@@ -141,7 +141,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: 'config.googleAnalyticsID',
+        trackingId: config.googleAnalyticsID,
       },
     },
   ],
